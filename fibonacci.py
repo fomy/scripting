ca82e6f2a710386d1423261774b88df36eae5f58
fomy <fumin@hust.edu.cn>
2014-07-15 10:12:34 +0800

    no

--- fibonacci.py
+++ fibonacci.py
@@ -4,9 +4,6 @@ import sys
 
 # "Think Python"
 
-# In my macbook pro with a 2.4GHz i5 processor,
-# fast_fibonacci is not faster than fibonacci when n < 40.
-
 def fibonacci(n):
     if n==0 or n==1:
         return n
@@ -17,7 +14,8 @@ known = {1:1,0:0}
 def fast_fibonacci(n):
     if n in known:
         return known[n]
-    return fast_fibonacci(n-1)+fast_fibonacci(n-2)
+    known[n] = fast_fibonacci(n-1)+fast_fibonacci(n-2)
+    return known[n]
 
 if sys.argv[2] == 'fast':
     print fast_fibonacci(int(sys.argv[1]))
