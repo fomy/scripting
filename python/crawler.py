2318e1d822d87c2e3e10a35c0f6b1dd6f641d2c4
fomy <fumin@hust.edu.cn>
2014-11-04 08:24:20 +0800

    exception handler for ContentTooShortError

--- python/crawler.py
+++ python/crawler.py
@@ -4,15 +4,30 @@ import urllib as ul
 import re
 
 path='http://tracer.filesystems.org/traces/macos/2014/'
+target='/home/dataset/mac2014/'
 root=ul.urlopen(path)
 content=root.read()
 match=re.compile(r'(?<=href=["]).*?(?=["])')
 names=re.findall(match, content)
 
+failed=[]
 for name in names:
     if name=='../':
         continue
-    
+    targetname=target+name
+    if os.path.exists(targetname):
+        print "skip %s"%name
+        continue
+
     fulpath=path+name
-    print fulpath
-    ul.urlretrieve(fulpath,"/home/dataset/mac2014/%s"%name)
+    print "downloading %s"%fulpath
+    try:
+        ul.urlretrieve(fulpath,targetname)
+    except urllib.ContentTooShortError:
+        print "failed %s"%name
+        failed.append(name)
+        continue
+
+
+print "all these files failed"
+print failed 
