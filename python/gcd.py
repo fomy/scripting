5016c36e8c981818d63f170e3b4d3ab878577767
fomy <fumin@hust.edu.cn>
2014-11-26 15:42:52 +0800

    least common multiply

--- python/gcd.py
+++ python/gcd.py
@@ -1,5 +1,7 @@
 import sys
-def gcd(x,y):
+
+# calculate the greatest common divisor
+def gcd(x,y): 
     if x==0:
         return y
     if y==0:
@@ -17,5 +19,10 @@ def gcd2(x,y):
         return gcd(x, y-x)
     return gcd(x-y,y)
 
+# calculate the least common multiply
+def lcm(x,y):
+    return x*y/gcd(x,y)
+
 print gcd(int(sys.argv[1]), int(sys.argv[2]))
 print gcd2(int(sys.argv[1]), int(sys.argv[2]))
+print lcm(int(sys.argv[1]), int(sys.argv[2]))
